e204fa2d92d94c97d15a4d1bf5b1d2dc6a0fb20a
johannes <jmuljana@gmail.com>
2020-08-24 18:05:55 +1000

    added HomeScreen

---
+++ components/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+export const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
